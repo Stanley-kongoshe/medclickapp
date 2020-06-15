9aa20fe83b7a56795f8c51084d2e7e9acec1d2b6
techwiz-dev <techwizgang@gmail.com>
2020-06-15 17:28:19 +0200

    initial commit

---
+++ medclickapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def homepage(request):
+    return render(request, 'medclickapp/index.html')
+
+
+def login_page(request):
+    return render(request, 'medclickapp/login.html')
+
+
+def signup_page(request):
+    return render(request, 'medclickapp/signup.html')
