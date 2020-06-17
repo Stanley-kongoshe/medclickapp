f833d81788ec16cb3299a42a6ae1aca5c9fcfb4e
techwiz-dev <techwizgang@gmail.com>
2020-06-17 20:50:18 +0200

    added login and signup backened

---
+++ users/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm
+# Create your views here.
+
+# The registration page view function
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.clean_username()
+        messages.success(
+            request, f"Your account has been created! You are now able to login")
+        return redirect('login')
+
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
